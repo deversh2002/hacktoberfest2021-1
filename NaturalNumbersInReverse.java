553a590fc21cc5b7379218dcc43e45137a6d5509
deversh2002 <77965457+deversh2002@users.noreply.github.com>
2021-10-13 21:54:01 +0530

    Add files via upload

---
+++ NaturalNumbersInReverse.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class NaturalNumbersInReverse {
+    public static void main(String[] args) {
+        int n = 0;
+        System.out.println("Here are your natural numbers");
+        for (int i = 100; i > n; i-- ){
+            System.out.println(i);
+        }
+    }
+}
